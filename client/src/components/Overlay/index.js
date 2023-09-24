cb5480cfae8ba7f90a3840a7de8957537e4530dc
Kapalik Khanal <kapalikkhanal@gmail.com>
2023-09-24 13:43:24 +0545

    Reomval of right click to inspect

--- client/src/components/Overlay/index.js
+++ client/src/components/Overlay/index.js
@@ -1,32 +1,65 @@
 import React, { useState, useEffect } from 'react';
 
 function BlackOverlay() {
-    const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [textPosition, setTextPosition] = useState({ x: 0, y: 0 });
 
-    useEffect(() => {
-        const handleMouseMove = (e) => {
-            setCursorPosition({ x: e.clientX, y: e.clientY });
-        };
-
-        window.addEventListener('mousemove', handleMouseMove);
-
-        return () => {
-            window.removeEventListener('mousemove', handleMouseMove);
-        };
-    }, []);
-
-    const overlayStyle = {
-        // Use cursorPosition to set the transparent circle centered at the cursor
-        background: `radial-gradient(circle at ${cursorPosition.x}px ${cursorPosition.y}px, transparent 0%, transparent 10%, rgba(0, 0, 0, 1) 11%)`,
+  useEffect(() => {
+    const handleMouseMove = (e) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
     };
 
-    return (
-        <>
-            <div className="fixed top-0 left-0 w-full h-full bg-[#000000cc] pointer-events-none" style={overlayStyle}>
+    window.addEventListener('mousemove', handleMouseMove);
 
-            </div>
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
 
-        </>
-    );
+  useEffect(() => {
+    // Update the text position based on the cursor position
+    const updateTextPosition = () => {
+      const newX = cursorPosition.x + 30; // Adjust the horizontal offset as needed
+      const newY = cursorPosition.y - 10; // Adjust the vertical offset as needed
+      setTextPosition({ x: newX, y: newY });
+    };
+
+    // Call the updateTextPosition function on initial load and every 50ms
+    updateTextPosition();
+    const intervalId = setInterval(updateTextPosition, 50);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [cursorPosition]);
+
+  const overlayStyle = {
+    // Use cursorPosition to set the transparent circle centered at the cursor
+    background: `radial-gradient(circle at ${cursorPosition.x}px ${cursorPosition.y}px, transparent 0%, transparent 10%, rgba(0, 0, 0, 1) 11%)`,
+  };
+
+  const textOverlayStyle = {
+    // Position the text using textPosition
+    position: 'fixed',
+    top: `${textPosition.y}px`,
+    left: `${textPosition.x}px`,
+    color: 'white',
+    fontSize: '24px', // Adjust the font size as needed
+    transition: 'top 0.5s ease-out, left 0.5s ease-out', // Add a smooth transition
+  };
+
+  return (
+    <>
+      <div className="fixed top-0 left-0 w-full h-full bg-[#000000cc] pointer-events-none" style={overlayStyle}></div>
+      
+      {/* Add a div for the "TRDM" text with a mask */}
+      <div className="fixed" style={textOverlayStyle}>
+        <div style={{ maskImage: overlayStyle.background }}>
+          The Real Dark Mode
+        </div>
+      </div>
+    </>
+  );
 }
+
 export default BlackOverlay;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,16 +1,68 @@
-import React from 'react'
-import BlackOverlay from '@/components/Overlay'
-import NavBar from '@/components/Navbar'
-import MazeApp from './MazeApp'
+import React, { useEffect, useState } from 'react';
+import BlackOverlay from '@/components/Overlay';
+import NavBar from '@/components/Navbar';
+import MazeApp from './MazeApp';
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
 
 function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Function to check if the screen width is below a certain threshold (e.g., 768 pixels for common mobile devices)
+    const checkIsMobile = () => {
+      setIsMobile(window.innerWidth < 768); // Adjust the threshold as needed
+    };
+
+    // Initial check when the component mounts
+    checkIsMobile();
+
+    // Add a resize event listener to update the isMobile state if the screen size changes
+    window.addEventListener('resize', checkIsMobile);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('resize', checkIsMobile);
+    };
+  }, []);
+
+  useEffect(() => {
+    // Add an event listener to disable the context menu on the entire document
+    const disableContextMenu = (e) => {
+      e.preventDefault();
+    };
+
+    document.addEventListener('contextmenu', disableContextMenu);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('contextmenu', disableContextMenu);
+    };
+  }, []); // Empty dependency array ensures this effect runs only once
+
   return (
-    <>
+    <div>
       <BlackOverlay />
       <NavBar />
-      <MazeApp />
-    </>
-  )
+      {isMobile ? (
+        <div className='text-2xl text-center flex justify-center text-red-800'>
+          <Alert status='error'>
+            <AlertIcon />
+            <div className='w-full flex flex-col justify-center items-center'>
+                <AlertTitle>Phone browsing is not supported!</AlertTitle>
+                <AlertDescription>Use laptop/tablet to open this site.</AlertDescription>
+            </div>
+          </Alert>
+        </div>
+      ) : (
+        <MazeApp />
+      )}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
